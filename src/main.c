f4402fed86cc5f4030be8afc798e1c9fb9e82589
staszczm <maciek.staszczyk1@gmail.com>
2022-12-13 03:47:38 +0100

    zwiekszenie czytelnosci pokazywanych wynikow

--- src/main.c
+++ src/main.c
@@ -13,20 +13,20 @@ int main(int argc, char **argv) {
 
     if (A == NULL) return -1;
     if (b == NULL) return -2;
+    
+    printf("Wczytana macierz A:\n");
     printToScreen(A);
+    printf("Wczytana macierz b:\n");
     printToScreen(b);
 
     res = eliminate(A, b);
-
-    //testowe wyswietlanie
-    printToScreen(A);
-    printToScreen(b);
 
     if (res == 0) {
         x = createMatrix(b->r, 1);
         if (x != NULL) {
             res = backsubst(x, A, b);
-
+            
+            printf("Macierz wynikow X:\n");
             printToScreen(x);
             freeMatrix(x);
         } else {
