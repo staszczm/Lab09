3939d4ba5b7f5c2b80182bed136a7a4c652b09b5
chmieladr <01178824@pw.edu.pl>
2022-12-13 18:43:42 +0100

    Implementacja elementu dominujacego - proba nr 2

--- src/gauss.c
+++ src/gauss.c
@@ -1,1 +1,60 @@
-/tmp/VirtualBox Dropped Files/2022-12-13T17:36:31.232994000Z/gauss.c
+#include <stdio.h>
+#include "gauss.h"
+
+/**
+ * Zwraca 0 - elimnacja zakonczona sukcesem
+ * Zwraca 1 - macierz osobliwa - dzielenie przez 0
+ * Zwraca 2 - wiecej niewiadomych niz rownan, a wiec brak jednoznacznej odpowiedzi
+ * Zwraca 3 - macierz wyrazow wolnych ma zbyt wiele kolumn
+ */
+
+double absolute(double x) {
+    if(x < 0)
+        return -x;
+    return x;
+}
+
+void swap(double *a, double *b) {
+    double *temp = a;
+    a = b;
+    b = temp;
+}
+
+int eliminate(Matrix *mat, Matrix *b) {
+    if(mat->row > mat->column) {
+        fprintf(stderr, "Podano wiecej niewiadomych niz rownan,\n"
+                        "nie mozna ustalic jednoznacznej odpowiedzi.\n");
+        return 2;
+    }
+    if(b->column != 1) {
+        fprintf(stderr, "Nieprawidlowa liczba kolumn macierzy wyrazow wolnych.\n");
+        return 3;
+    }
+
+    int size = mat->row;
+    int column, row;
+
+    /// przeksztalcenie macierzy A w macierz gorna-trojkatna z wyborem elementu dominujaacego
+    for (column = 0; column < size - 1; column++) {
+        int column_max = column;
+        for (row = column + 1; row < size; row++)
+            if(absolute(mat->data[row][column]) > absolute(mat->data[column_max][column]))
+                column_max = row;
+        if(column_max != column) {
+            swap(mat->data[column], mat->data[column_max]);
+            swap(b->data[column], b->data[column_max]);
+        }
+        for (row = column + 1; row < size; row++) {
+            if (mat->data[column][column] == 0) {
+                fprintf(stderr, "Podana macierz jest osobliwa!\n");
+                return 1;
+            }
+            double temp = mat->data[row][column] / mat->data[column][column];
+            int i;
+            for (i = column; i < size; i++)
+                mat->data[row][i] -= temp * mat->data[column][i];
+            b->data[row][0] -= temp * b->data[column][0];
+        }
+    }
+    return 0;
+}
